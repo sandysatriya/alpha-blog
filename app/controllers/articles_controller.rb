6c7f6c2d3637b81268ab4cc791f6574ff46cd4a0
Sandy Satriya <sandy@multiinti.com>
2020-05-12 01:41:07 +0700

    fixed bug on delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destory]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
     # byebug => debugger
