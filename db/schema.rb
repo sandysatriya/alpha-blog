4870c061636bd2af7c6e3977f696c30cfd5a2860
Sandy Satriya <sandy@multiinti.com>
2020-05-12 20:30:01 +0700

    add validation if update_at or create_at nil

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_131517) do
+ActiveRecord::Schema.define(version: 2020_05_12_131651) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
