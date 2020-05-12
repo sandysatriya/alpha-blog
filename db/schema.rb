cd6c1536851c77cbd30c13f598d70a3f5b9264c7
Sandy Satriya <sandy@multiinti.com>
2020-05-12 19:49:19 +0700

    layouting edit form and flash message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_141753) do
+ActiveRecord::Schema.define(version: 2020_05_12_124703) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
